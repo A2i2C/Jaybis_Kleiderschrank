50ac32bc28fb246f88cbc6f2a715cbc9f4e6f4dc
Aziz Carducci <carducciaziz@gmail.com>
2025-12-16 13:47:56 +0100

    feat(camera): Implement camera functionality with permission handling

---
+++ app/src/main/java/com/example/kleiderschrank/CameraHandler.kt
@@ -0,0 +1,38 @@
+package com.example.kleiderschrank
+
+import android.content.Context
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import android.os.Environment
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class CameraHandler {
+    lateinit var photoFile : File
+        private set
+
+    fun createCameraIntent(context: Context) : Intent {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        photoFile = createImageFile(context) // To be implemented: create a method to create an image file
+        val photoURI = FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.fileprovider",
+            photoFile
+        )
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        return intent
+    }
+
+    private fun createImageFile(context: Context) : File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.GERMANY).format(Date())
+        val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "IMG_${timeStamp}_", ".jpg", storageDir
+        )
+    }
+}
--- app/src/main/java/com/example/kleiderschrank/MainActivity.kt
+++ app/src/main/java/com/example/kleiderschrank/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.kleiderschrank
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCamera: Button
+    private lateinit var cameraHandler: CameraHandler
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    private lateinit var cameraPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +31,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        cameraHandler = CameraHandler()
+        btnCamera = findViewById(R.id.btnCamera)
+
+        initCameraLaunchers()
+
+        btnCamera.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
     }
+    private fun openCamera() {
+        val cameraIntent = cameraHandler.createCameraIntent(this)
+        cameraLauncher.launch(cameraIntent)
+    }
+
+    private fun initCameraLaunchers() {
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result ->
+            if (result.resultCode == RESULT_OK) {
+                Log.i("Image saved", "Image saved at: ${cameraHandler.photoFile.absolutePath}")
+            }
+        }
+        cameraPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission())
+        { granted ->
+            if (granted) {
+                openCamera()
+            }
+        }
+    }
+
+
+
 }
